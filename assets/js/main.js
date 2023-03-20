588db1f3452635367738e92c8c3e3937fa1cbccd
ALEXANDER <msumba83@gmail.com>
2023-03-20 17:02:58 +0200

    done footer and added JavaScript to scroll active nav links

--- assets/js/main.js
+++ assets/js/main.js
@@ -132,3 +132,26 @@ inputs.forEach((input) => {
   input.addEventListener("blur", blurFunc);
 });
 
+// ========= SCROLL SECTION ACTIVE LINKS ========
+// getting all sections that has an ID defined 
+const sections = document.querySelectorAll("section[id]");
+
+window.addEventListener("scroll", navHighlighter);
+
+function navHighlighter() {
+  let scrollY = window.pageYOffset;
+
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50,
+    sectionId = current.getAttribute("id");
+
+    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight)
+    {
+       document.querySelector(".nav-menu a[href*=" + sectionId + "] ").classList.add("active-link");
+    } else {
+      document.querySelector('.nav-menu a[href*=' + sectionId + '] ' ).classList.remove("active-link")
+    }
+  })
+}
+
